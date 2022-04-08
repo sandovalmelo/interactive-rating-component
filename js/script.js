76ba17b30906219ac2d3fa3ac15568a5ad238fd5
Sandoval Melo <juniormelo777@gmail.com>
2022-04-08 17:50:57 -0300

    added rating

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 const ratingBtns = Array.from(document.querySelectorAll(".rating-btn"));
+const submitBtn = document.getElementById("submit-btn");
+const ratingState = document.getElementById("rating-state");
+const thanksState = document.getElementById("thanks-state");
+const ratingSelected = document.querySelector(".rating-selected");
 
 let selectRating;
 
 function selectBtn(event) {
 	const btnTarget = event.target;
+	let numberSelected = btnTarget.innerText;
 
 	ratingBtns.forEach((btn) => {
 		if (btn !== btnTarget) {
 			btn.classList.remove("selected");
 		} else {
-			btnTarget.classList.toggle("selected");
+			if (btnTarget.classList.contains("selected")) {
+				btnTarget.classList.remove("selected");
+				numberSelected = 0;
+			} else {
+				btnTarget.classList.add("selected");
+			}
 		}
 	});
 
-	console.dir(btnTarget.innerText);
+	selectRating = numberSelected;
+}
+
+function submitRating() {
+	console.log(selectRating);
+	if (selectRating) {
+		ratingState.hidden = true;
+		thanksState.hidden = false;
+		ratingSelected.innerText = selectRating;
+	}
 }
 
 ratingBtns.forEach((btn) => {
 	btn.addEventListener("click", selectBtn);
 });
+
+submitBtn.addEventListener("click", submitRating);
